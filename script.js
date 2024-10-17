194b9ea3e11468963aa51e894f2c41b667b9c17b
Sean <seanselbyy@gmail.com>
2024-10-17 13:57:14 +1300

    Added blue color change function when user presses blue button

--- script.js
+++ script.js
@@ -17,3 +17,18 @@ const logButton = document.getElementById('log-button');
 logButton.addEventListener('click', function() {
     console.log(timeDisplay.innerText);
 });
+
+const blueButton = document.getElementById('blue-button');
+
+const alertChangeBlue = document.getElementById('alert-button');
+const logChangeBlue = document.getElementById('log-button');
+const blueChangeBlue = document.getElementById('blue-button');
+const whiteChangeBlue = document.getElementById('white-button');
+
+
+blueButton.addEventListener('click', function() {
+    alertChangeBlue.style.backgroundColor = 'blue';
+    logChangeBlue.style.backgroundColor = 'blue';
+    blueChangeBlue.style.backgroundColor = 'blue';
+    whiteChangeBlue.style.backgroundColor = 'blue';
+});
